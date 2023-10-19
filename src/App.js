c469caeb9207be2fe748fee53a35c5d464a37201
mukulls <mukulshrma90@gmail.com>
2023-10-19 12:51:58 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom"; //imported for routing the website from one page to another
+import Home from "./pages/Home/Home"; //importing Home Folder which has Home.js file
+import Product from "./pages/Product/Product"; //importing Product.js
+import Products from "./pages/Products/Products"; //import products.js
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/NAvbar";
+
+const Layout =()=>{
+  return(
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([ //creating router
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>
+      }
+    ]
+  },
+  {
+    path:"/", //path to home
+    element:<span>Home</span> //route to home
+  },
+  {
+    path:"/products/:id", //path to product category
+    element:<span>Category</span>
+  },
+  {
+    path:"/product/:id", //path to single product
+    element:<span>Product</span>
+  }
+]
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <RouterProvider router = {router} />
+    </>
   );
 }
 
