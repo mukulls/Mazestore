7b75f1033bd52b7c5c407b51ef4a7115652780dd
mukulls <mukulshrma90@gmail.com>
2023-10-26 23:03:20 +0530

    day 3

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import {BsArrowLeftCircle} from "react-icons/bs";
+import {BsArrowRightCircle} from "react-icons/bs";
+import "./Slider.scss";
+const Slider= ()=>{
+  const[currentSlide, setCurrentSlide] = useState(0);
+    const data=[
+       "https://images.pexels.com/photos/7081113/pexels-photo-7081113.jpeg",
+       "https://images.pexels.com/photos/4672559/pexels-photo-4672559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      "https://images.pexels.com/photos/5119917/pexels-photo-5119917.jpeg"
+    ];
+    const prevSlide=()=>{
+      setCurrentSlide(currentSlide === 0?2 : (prev) => prev -1)
+    }
+    const nextSlide=()=>{
+      setCurrentSlide(currentSlide === 2?0 : (prev) => prev +1)
+    }
+
+    return (
+      <div className="slider">
+        <div className='container' style={{transform: `translateX(-${currentSlide *100}vw)`}}>
+          <img src= {data[0]} alt=""/>
+          <img src= {data[1]} alt=""/>
+          <img src= {data[2]} alt=""/>
+        </div>
+        <div className="icons">
+          <div className='icon' onClick={prevSlide}>
+            <BsArrowLeftCircle/>
+          </div>
+          <div className='icon' onClick={nextSlide}>
+            <BsArrowRightCircle/>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Slider
