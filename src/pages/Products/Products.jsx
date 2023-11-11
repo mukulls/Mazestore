9d718d5546106c05b5796f03c76ed5407f5f7960
mukulls <mukulshrma90@gmail.com>
2023-11-11 20:41:13 +0530

    dayNA

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,7 +2,52 @@ import React from 'react'
 import "./Products.scss";
 const Products = () => {
   return (
-    <div className='products'>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1}/>
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2}/>
+            <label htmlFor="2">Cargo Pants</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3}/>
+            <label htmlFor="3">Coat</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="4" value={4}/>
+            <label htmlFor="4">Biker Jacket</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={10000} />
+            <span>10000</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id='asc' value='asc' name='price' />
+            <label htmlFor="asc">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id='desc' value='desc' name='price' />
+            <label htmlFor="desc">Price (Highest first)</label>
+          </div>
+        </div>
+        </div>
+        <div className="right">
+          <img className='catImg'
+           src="" alt="" />
+        </div>
+      </div>
   )
 }
 
